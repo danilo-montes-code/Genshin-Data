fef8e9500d14f5257a4a5474922f9824cbbdd049
danilo-montes <dem9855@nyu.edu>
2022-03-25 12:49:46 -0400

    fixed indentation in db.js

--- db.js
+++ db.js
@@ -11,30 +11,30 @@ const User = new mongoose.Schema({
 
 const Character = new mongoose.Schema({
   name            : {type: String, required: true},
-	region          : {type: String, required: true},
-	vision          : {type: String, required: true},
-	talent_material : {type: mongoose.Schema.Types.ObjectId, ref: 'Material', required: true},
-	weekly_material : {type: mongoose.Schema.Types.ObjectId, ref: 'Material', required: true}
+  region          : {type: String, required: true},
+  vision          : {type: String, required: true},
+  talent_material : {type: mongoose.Schema.Types.ObjectId, ref: 'Material', required: true},
+  weekly_material : {type: mongoose.Schema.Types.ObjectId, ref: 'Material', required: true}
 });
 
 const Weapon = new mongoose.Schema({
   name               : {type: String, required: true},
   class              : {type: String, required: true},
   rarity             : {type: Number, required: true},
-	stats              : {type: Object, required: true},
-	ascension_material : {type: mongoose.Schema.Types.ObjectId, ref: 'Material', required: true},
+  stats              : {type: Object, required: true},
+  ascension_material : {type: mongoose.Schema.Types.ObjectId, ref: 'Material', required: true},
 });
 
 const Material = new mongoose.Schema({
   name         : {type: String, required: true},
   days_of_week : {type: String},
-	domain       : {type: mongoose.Schema.Types.ObjectId, ref: 'Domain', required: true},
+  domain       : {type: mongoose.Schema.Types.ObjectId, ref: 'Domain', required: true},
 });
 
 const Domain = new mongoose.Schema({
   name         : {type: String, required: true},
   region       : {type: String, required: true},
-	weekly_boss  : {type: String}
+  weekly_boss  : {type: String}
 });
 
 
