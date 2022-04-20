01b3b1c4f6d1d342098dde0d4701e02213f6b68e
danilo-montes <dem9855@nyu.edu>
2022-04-19 21:06:23 -0400

    google login seems to work

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -2,31 +2,17 @@ let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  id: {
-    type: String,
-    unique: true
-  },
-  firstName: String,
-  lastName: String,
-  profilePhoto: String,
-  password: String,
-  source: { type: String, required: [true, "source not specified"] },
-  lastVisited: { type: Date, default: new Date() },
+  id           : {type: String, unique: true},
+  firstName    : String,
+  lastName     : String,
+  profilePhoto : String,
+  password     : String,
+  source       : { type: String, required: [true, "source not specified"] },
+  lastVisited  : { type: Date, default: new Date() },
 
-  characters : [{type: mongoose.Schema.Types.ObjectId, ref: 'Character'}],
-  weapons    : [{type: mongoose.Schema.Types.ObjectId, ref: 'Weapon'}]
+  characters   : [{type: mongoose.Schema.Types.ObjectId, ref: 'Character'}],
+  weapons      : [{type: mongoose.Schema.Types.ObjectId, ref: 'Weapon'}]
   // pollAnswers : [{type: String} : {type: mongoose.Schema.Types.ObjectId, ref: 'PollAnswer'}]
 });
 
-// const User = new mongoose.Schema({
-//   // username, password
-//   characters : [{type: mongoose.Schema.Types.ObjectId, ref: 'Character'}],
-//   weapons    : [{type: mongoose.Schema.Types.ObjectId, ref: 'Weapon'}]
-//   // poll_answers : [{type: String} : {type: mongoose.Schema.Types.ObjectId, ref: 'PollAnswer'}]
-// });
-
-
-
-const userModel = mongoose.model('User', userSchema);
-
-module.exports = userModel;
+module.exports = mongoose.model('User', userSchema);
