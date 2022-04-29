7157f9153503ba5c103b79a98df3a30a8d5f92b5
danilo-montes <dem9855@nyu.edu>
2022-04-28 20:01:57 -0400

    testing date prints

--- src/routes/index.js
+++ src/routes/index.js
@@ -98,6 +98,10 @@ router.get('/', async (req, res) =>  {
         tomorrow = fns.subHours(fns.startOfTomorrow(today), getRegionOffset(timeZone)),
         nextWeek = fns.subHours(fns.startOfDay(fns.nextMonday(today)), getRegionOffset(timeZone));
 
+  console.log(timeZone);
+  console.log(tomorrow);
+  console.log(nextWeek);
+
   let dhours   = fns.differenceInHours(tomorrow, today) % 24,
       dminutes = fns.differenceInMinutes(tomorrow, today) % 60,
       dseconds = fns.differenceInSeconds(tomorrow, today) % 60,
